20a4ad08b05e8b9e66519d922296c8b2bc38375b
gulistan saglam <gulistansglm@gmail.com>
2025-05-06 00:31:55 +0300

    changes made2

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
-import express from 'express'
-import dotenv from "dotenv"
+import express from 'express';
+import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import authRoutes from './src/routes/auth.route.js'
-import messageRoutes from './src/routes/message.route.js'
+import authRoutes from './src/routes/auth.route.js';
+import messageRoutes from './src/routes/message.route.js';
 import { connectDB } from './src/lib/db.js';
 import { app, server } from './src/lib/socket.js';
 
 import path from "path";
 
-dotenv.config()
-const PORT = process.env.PORT
-const __dirname = path.resolve()
+dotenv.config();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
-}))
+}));
 app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes)
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
